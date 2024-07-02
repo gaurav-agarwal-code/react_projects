445912d47ad22361bd5e5793ad37b47e0532d106
gaurav-agarwal-code <986gauravagarwal@gmail.com>
2024-07-02 21:16:04 +0530

    added first project

---
+++ 03counteronclick/clickcounter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15)
+
+  const upcount = (props) => {
+    // setCounter(counter + 1) .........will not work since this all cmd is  send in batch
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    setCounter(prevCounter => prevCounter + 1) //.......first take previous state of counter as callback and then increment it as required
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+  }
+
+  const downcount = () => setCounter(counter - 1)
+
+  return (
+    <>
+      <p>click accordingly</p>
+      <button
+        onClick={upcount}>Addcount {counter}</button>
+      <button
+        onClick={downcount}>Downcount {counter}</button>
+    </>
+  )
+}
+
+export default App
